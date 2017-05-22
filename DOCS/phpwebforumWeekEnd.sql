a0a866874c2ebd55f2b7d12b93853eeaf918d004
Matthew Stinis <mpstinis@gmail.com>
2017-05-22 18:24:18 -0400

    *project weekend update, new page templates, partial php code

---
+++ DOCS/phpwebforumWeekEnd.sql
@@ -0,0 +1,168 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 23, 2017 at 12:22 AM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `phpwebforum`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `boards`
+--
+
+CREATE TABLE `boards` (
+  `boardId` int(11) NOT NULL,
+  `title` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `boards`
+--
+
+INSERT INTO `boards` (`boardId`, `title`) VALUES
+(1, 'Musictalk'),
+(2, 'Geartech'),
+(3, 'Events');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `postId` int(11) NOT NULL,
+  `threadId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `body` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `threads`
+--
+
+CREATE TABLE `threads` (
+  `threadId` int(11) NOT NULL,
+  `boardId` int(11) NOT NULL,
+  `title` varchar(250) NOT NULL,
+  `date` date NOT NULL,
+  `replies` int(250) NOT NULL,
+  `views` int(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `userId` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userId`, `username`, `password`) VALUES
+(2, 'mstinis', 'abC123'),
+(3, 'lalala', 'abC123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `boards`
+--
+ALTER TABLE `boards`
+  ADD PRIMARY KEY (`boardId`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`postId`),
+  ADD KEY `threadId` (`threadId`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indexes for table `threads`
+--
+ALTER TABLE `threads`
+  ADD PRIMARY KEY (`threadId`),
+  ADD KEY `boardId` (`boardId`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userId`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `boards`
+--
+ALTER TABLE `boards`
+  MODIFY `boardId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `postId` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `threads`
+--
+ALTER TABLE `threads`
+  MODIFY `threadId` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`threadId`) REFERENCES `threads` (`threadId`),
+  ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`);
+
+--
+-- Constraints for table `threads`
+--
+ALTER TABLE `threads`
+  ADD CONSTRAINT `threads_ibfk_1` FOREIGN KEY (`boardId`) REFERENCES `boards` (`boardId`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- project.php
+++ project.php
@@ -14,10 +14,10 @@ $log->pushHandler(new StreamHandler('logs/everything.log', Logger::DEBUG));
 $log->pushHandler(new StreamHandler('logs/errors.log', Logger::ERROR));
 
 DB::$host = '127.0.0.1';
-DB::$user = 'PHPwebforum';
+DB::$user = 'phpwebforum';
 DB::$password = '5zAijLF4Ooaojs6O';
 DB::$dbName = 'phpwebforum';
-DB::$port = '3333';
+DB::$port = '3306';
 DB::$encoding = 'utf8';
 
 DB::$error_handler = 'sql_error_handler';
@@ -138,8 +138,6 @@ $app->post('/login', function() use ($app) {
     }
 });
 
-
-
 $app->get('/logout', function() use ($app) {
     unset($_SESSION['user']);
     $app->render('logout.html.twig');
@@ -152,33 +150,108 @@ $app->get('/board', function() use ($app) {
     $app->render('board_view.html.twig');
 });
 
-// list of threads in a board
-$app->get('/board/:boardId', function() {
-    
+//// list of threads in a board
+//$app->get('/board(/:boardId)', function($id) use ($app) {
+//   $thread = DB::queryFirstRow('SELECT * FROM boards WHERE boardId=%i', $id);
+//   $app->render('board_view.html.twig', array(
+//        't' => $thread
+//    ));
+//    
+//});
+
+
+$app->get('/board/musictalk', function() use ($app) {
+    DB::insertUpdate('boards', array(
+        'boardId' => '1',
+        'title' => 'Musictalk'
+    ));
+    $app->render('board_musictalk.html.twig');
+});
+
+$app->get('/board/geartech', function() use ($app) {
+    DB::insertUpdate('boards', array(
+        'boardId' => '2',
+        'title' => 'Geartech'
+    ));
+    $app->render('board_geartech.html.twig');
+});
+
+$app->get('/board/events', function() use ($app) {
+    DB::insertUpdate('boards', array(
+        'boardId' => '3',
+        'title' => 'Events'
+    ));
+    $app->render('board_events.html.twig');
 });
 
 // view of one thread on a board
-$app->get('/thread/:threadId', function($threadId) {
+$app->get('/thread/:threadId', function($threadId) use ($app) {
     $postList = DB::query("SELECT * FROM posts WHERE boardId=%i AND threadId=%", $threadId);
     $app->render('thread_view.html.twig', array('postList' => $postList));
 });
 // 3-state form to create new thread in a board
-$app->get('/board/:boardId/newThread', function() {
-    
+$app->get('/board/:boardId/newthread', function() use ($app) {
+    $app->render('new_thread.html.twig');
 });
-$app->post('/board/:boardId/newThread', function() {
+$app->post('/board/:boardId/newthread', function() use ($app) {
+    $title = $app->request()->post('title');
+    $date = $app->request()->post('date');
+    $postList = array('title' => $title, 'date' => $date);
+    // verify inputs
+    $errorList = array();
+    if (strlen($title) < 2 || strlen($title) > 100) {
+        array_push($errorList, "Title must be between 2 and 100 characters");
+    }
+    // TODO: generate date according to date of thread creation
     
+    // receive data and insert
+    if (!$errorList) {
+        $boardId = $_SESSION['user']['id'];
+        DB::insert('thread', array(
+            'boardId' => $boardId,
+            'title' => $title,
+            'date' => $date
+        ));
+        $app->render('new_thread_success.html.twig');
+    } else {
+        $app->render('new_thread.html.twig', array(
+            'p' => $postList
+        ));
+    }
 });
 // 3-state form to reply to a thread
-$app->get('/thread/:threadId/reply', function() {
-    
+$app->get('/thread/:threadId/reply', function() use ($app) {
+    $app->render('view_thread.html.twig');
 });
-$app->get('/thread/:threadId/reply', function() {
+$app->post('/thread/:threadId/reply', function() use ($app) {
+    $title = $app->request()->post('title');
+    $body = $app->request()->post('body');
+    $postList = array('title' => $title, 'body' => $body);
+    // verify inputs
+    $errorList = array();
+    if (strlen($title) < 2 || strlen($title) > 100) {
+        array_push($errorList, "Title must be between 2 and 100 characters");
+    }
+    if (strlen($body) < 1) {
+        array_push($errorList, "Body cannot be empty when replying to a thread");
+    }
+    // TODO: generate date according to date of reply/post
     
+    // receive data and insert
+    if (!$errorList) {
+        $threadId = $_SESSION['user']['id'];
+        DB::insert('thread', array(
+            'threadId' => $threadId,
+            'title' => $title,
+            'body' => $body
+        ));
+        $app->render('new_reply_success.html.twig');
+    } else {
+        $app->render('new_reply.html.twig', array(
+            'p' => $postList
+        ));
+    }
 });
-
-
-
 
 
 $app->run();
